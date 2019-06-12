f17e773f1b0d2a17df8ab1c4c508bdf8faae8908
Luke Phyall <shoreditchwarlock@gmail.com>
2019-06-12 16:41:24 +0100

    Parser process method correctly returns hash values and refactors tests

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,8 +1,14 @@
 class Parser
 
   def process(data)
-    processed = [{}]
-
+    processed = []
+    split_log = data.split("\n")
+    split_log.each { |element|
+      split_element = element.split(" ")
+      hash = { :url => split_element[0], :ip => split_element[1] }
+      processed << hash
+    }
+    processed
   end
 
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -6,12 +6,11 @@ describe Parser do
   let (:data) { "/home 235.313.352.950\n/help_page/1 451.106.204.921\n/index 451.106.204.921" }
 
   context '#process' do
-    it "process method outputs an array" do
-      expect(parser.process(data)).to be_an(Array)
+
+    it "method returns correct url and ip from array of hashes" do
+      expect(parser.process(data)[0][:url]).to eq('/home')
+      expect(parser.process(data)[2][:ip]).to eq('451.106.204.921')
     end
 
-    it "method outputs an array of hashes" do
-      expect(parser.process(data)[0]).to be_a(Hash)
-    end
   end
 end
