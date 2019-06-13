685945a13cad7d0dd9aff10d3aae29e12eef0ec9
Luke Phyall <shoreditchwarlock@gmail.com>
2019-06-13 15:27:03 +0100

    Adds #total_views to Processor

--- lib/processor.rb
+++ lib/processor.rb
@@ -1,7 +1,12 @@
 class Processor
 
-  def total_views
-
+  def total_views(data)
+    urls = []
+    data.each { |hash| urls << hash[:url] }
+    total = urls.uniq
+    total.map { |url|
+      "#{url}: #{urls.count(url)} view(s)"
+    }
   end
 
 end
--- spec/processor_spec.rb
+++ spec/processor_spec.rb
@@ -3,11 +3,19 @@ require './lib/processor'
 describe Processor do
 
   let (:processor) { Processor.new }
+  let (:data) {
+    [
+      {url: "/about", ip: "451.106.204.921"},
+      {url: "/about/2", ip: "382.335.626.855"},
+      {url: "/about/2", ip: "316.433.849.805"}
+    ]
+  }
 
   context "#total_views" do
-
-    it "has a total_views method" do
-      expect(processor).to respond_to :total_views 
+    
+    it "builds a counted array" do
+      expect(processor.total_views(data)).to eq(["/about: 1 view(s)", "/about/2: 2 view(s)"])
     end
+
   end
 end
